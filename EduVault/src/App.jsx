317295ec36407d28cb8d2f94b5512027685e4518
tatenda.tobaiwa <tatenda.tobaiwa@gmail.com>
2025-01-25 11:09:40 +0000

    push

--- EduVault/src/App.jsx
+++ EduVault/src/App.jsx
@@ -1,22 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home";
 import Institutions from "./pages/Institutions";
 import Scholarships from "./pages/Scholarships";
+import LandingPage from "./pages/Landing/Landing";
+import Signup from "./pages/Signup/Signup";
+import Signin from "./pages/Signin/Signin";
+import "./App.css";
 
 const hideHeaderFooterRoutes = ['/', '/signup', '/signin'];
 
-
 function App() {
+  const location = useLocation();
+  const shouldHideHeaderFooter = hideHeaderFooterRoutes.includes(location.pathname);
   return (
-    <Router>
+    <div className="EduVault">
+      {!shouldHideHeaderFooter && <Header />}
+      <main>
       <Routes>
-        <Route path="/" element={<Home />} />
+      <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
         <Route path="/institutions" element={<Institutions />} />
         <Route path="/scholarships" element={<Scholarships />} />
       </Routes>
-    </Router>
+      </main>
+      {!shouldHideHeaderFooter && <Footer />}
+    </div>
   );
 }
 
